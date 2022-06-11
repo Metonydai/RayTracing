ec4da1f471a977c71a953c44ecdfe1d3ec2aeb1f
Metonydai <kusuje@gmail.com>
2022-06-11 08:14:08 +0800

    2. Motion Blur

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include "camera.h"
 #include "material.h"
+#include "moving_sphere.h"
 
 hittable_list random_scene(){
     hittable_list world;
@@ -26,7 +27,9 @@ hittable_list random_scene(){
                     // diffuse
                     auto albedo = color::random() * color::random();
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    auto center2 = center + vec3(0, random_double(0,0.5), 0);
+                    world.add(make_shared<moving_sphere>(
+                                center, center2, 0.0, 1.0, 0.2, sphere_material));
                 }else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = color::random(0.5,1.0);
@@ -81,10 +84,10 @@ int main(){
     srand(seed);
 
 	// Image
-    const auto aspect_ratio = 3.0 / 2.0;
-	const int image_width = 1200;
+    const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 500;
+    const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     // World
@@ -117,7 +120,7 @@ int main(){
     auto dist_to_focus = 10.0;
     auto aperture = 0.1;
 
-    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus);
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
 	// Render
 	
