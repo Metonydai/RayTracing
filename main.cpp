2a8722c1028a60009fdcdb98ffed303a925b3e9e
KyoaniDai <kusuje@gmail.com>
2023-04-04 20:17:28 +0800

    6. Image Texture Mapping

--- main.cpp
+++ main.cpp
@@ -84,6 +84,14 @@ hittable_list two_perlin_spheres(){
 
 }
 
+hittable_list earth() {
+	auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+	auto earth_surface = make_shared<lambertian>(earth_texture);
+	auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+	
+	return hittable_list(globe);
+}
+
 color ray_color(const ray& r, const hittable& world, int depth){
     hit_record rec;
     
@@ -142,10 +150,18 @@ int main(){
 			vfov = 20.0;
 			break;
 
-		default:
 		case 3:
 			world = two_perlin_spheres();
 			lookfrom = point3(13,2,3);
+			lookat = point3(0,0,0);
+			vfov = 20.0;
+			break;
+
+		default:
+		case 4:
+			world = earth();
+			//lookfrom = point3(13,2,3);
+			lookfrom = point3(5,12,4);
 			lookat = point3(0,0,0);
 			vfov = 20.0;
 			break;
