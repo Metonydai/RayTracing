65b9e57d21cb236fa8eaa85245a14ce50fe779f4
KyoaniDai <kusuje@gmail.com>
2023-04-11 23:20:04 +0800

    8. Instances : Cornell Box with Two Blocks

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "camera.h"
 #include "material.h"
 #include "moving_sphere.h"
+#include "box.h"
 
 hittable_list random_scene(){
     hittable_list world;
@@ -120,6 +121,9 @@ hittable_list cornell_box() {
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
     objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    objects.add(make_shared<box>(point3(130, 0 ,65), point3(295, 165, 230), white));
+    objects.add(make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
 
     return objects;
 }
