4d0a6299f5e805a5cc3cd6d5501be5c7cf4a2704
KyoaniDai <kusuje@gmail.com>
2023-04-22 11:56:35 +0800

    2.2 Showing Convergence

--- pi.cpp
+++ pi.cpp
@@ -11,8 +11,10 @@ int main() {
     seed = (unsigned)time(NULL);
     srand(seed);
 
-	int N = 1000;
+	//int N = 1000;
 	int inside_circle = 0;
+	int runs = 0;
+	/*
 	for (int i = 0; i < N; i++) {
 		auto x = random_double(-1, 1);
 		auto y = random_double(-1, 1);
@@ -21,4 +23,18 @@ int main() {
 	}
 	std::cout << std::fixed << std::setprecision(12);
 	std::cout << "Estimate of pi = " << 4*double(inside_circle) / N << std::endl;
+	*/
+
+	std::cout << std::fixed << std::setprecision(12);
+	while(true) {
+		runs++;
+		auto x = random_double(-1, 1);
+		auto y = random_double(-1, 1);
+		if ( x*x+ y*y < 1)
+			inside_circle++;
+
+		if (runs % 10000 == 0)
+			std::cout << "Estimate of Pi = " << 4*double(inside_circle) / runs << std::endl;
+	}
+
 }
