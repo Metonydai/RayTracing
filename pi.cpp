3bcb6fc6adfb4fc9cba8a0558defd4fdf66d8998
KyoaniDai <kusuje@gmail.com>
2023-04-22 15:00:54 +0800

    2.3 Stratified Samples (Jittering)

--- pi.cpp
+++ pi.cpp
@@ -11,30 +11,26 @@ int main() {
     seed = (unsigned)time(NULL);
     srand(seed);
 
-	//int N = 1000;
+	int sqrt_N = 10000;
 	int inside_circle = 0;
-	int runs = 0;
-	/*
-	for (int i = 0; i < N; i++) {
-		auto x = random_double(-1, 1);
-		auto y = random_double(-1, 1);
-		if ( x*x+ y*y < 1)
-			inside_circle++;
+	int inside_circle_stratified = 0;
+	
+	for (int i = 0; i < sqrt_N; i++) {
+		for (int j = 0; j < sqrt_N; j++) {
+			auto x = random_double(-1, 1);
+			auto y = random_double(-1, 1);
+			if ( x*x + y*y < 1)
+				inside_circle++;
+			x = 2 * ((i+ random_double()) / sqrt_N) - 1;
+			y = 2 * ((j+ random_double()) / sqrt_N) - 1;
+			if ( x*x + y*y < 1)
+				inside_circle_stratified++;
+		}
 	}
-	std::cout << std::fixed << std::setprecision(12);
-	std::cout << "Estimate of pi = " << 4*double(inside_circle) / N << std::endl;
-	*/
 
+	auto N = static_cast<double>(sqrt_N) * sqrt_N;
 	std::cout << std::fixed << std::setprecision(12);
-	while(true) {
-		runs++;
-		auto x = random_double(-1, 1);
-		auto y = random_double(-1, 1);
-		if ( x*x+ y*y < 1)
-			inside_circle++;
-
-		if (runs % 10000 == 0)
-			std::cout << "Estimate of Pi = " << 4*double(inside_circle) / runs << std::endl;
-	}
+	std::cout << "Regular    Estimate of pi = " << 4*double(inside_circle) / (sqrt_N*sqrt_N) << std::endl;
+	std::cout << "Stratified Estimate of pi = " << 4*double(inside_circle_stratified) / (sqrt_N*sqrt_N) << std::endl;
 
 }
