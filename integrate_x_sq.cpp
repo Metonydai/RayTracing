b19acc863d84ed9ae8c318cf642252f6fc7564e8
KyoaniDai <kusuje@gmail.com>
2023-04-24 21:04:09 +0800

    3.3 Constructing a PDF : Integrating x2 with PDF

--- integrate_x_sq.cpp
+++ integrate_x_sq.cpp
@@ -4,13 +4,24 @@
 #include <iomanip>
 #include <math.h>
 
+inline double pdf(double x) {
+    return 0.5*x;
+}
+
 int main() {
+    // Random seed
+    unsigned seed;
+    seed = (unsigned)time(NULL);
+    srand(seed);
+
     int N = 1000000;
     auto sum = 0.0;
     for (int i = 0; i < N; i++) {
-        auto x = random_double(0,2);
-        sum += x*x;
+        auto x = sqrt(random_double(0,4));
+        if (x < 0.000001)
+            continue;
+        sum += x*x / pdf(x);
     }
     std::cout << std::fixed << std::setprecision(12);
-    std::cout << "I = " << 2 * sum / N << '\n';
+    std::cout << "I = " << sum / N << '\n';
 }
