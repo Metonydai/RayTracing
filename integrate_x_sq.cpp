6fc223d49f1a983a6cb64ab9a72f1e21f676afd2
KyoaniDai <kusuje@gmail.com>
2023-04-25 21:31:16 +0800

    3.4 Importance Sampling : Integrating x2, v3

--- integrate_x_sq.cpp
+++ integrate_x_sq.cpp
@@ -5,7 +5,8 @@
 #include <math.h>
 
 inline double pdf(double x) {
-    return 0.5*x;
+    //return 0.5*x;
+    return 0.5;
 }
 
 int main() {
@@ -17,9 +18,10 @@ int main() {
     int N = 1000000;
     auto sum = 0.0;
     for (int i = 0; i < N; i++) {
-        auto x = sqrt(random_double(0,4));
-        if (x < 0.000001)
-            continue;
+        //auto x = sqrt(random_double(0,4));
+        //if (x < 0.000001)
+            //continue;
+        auto x = random_double(0,2);
         sum += x*x / pdf(x);
     }
     std::cout << std::fixed << std::setprecision(12);
